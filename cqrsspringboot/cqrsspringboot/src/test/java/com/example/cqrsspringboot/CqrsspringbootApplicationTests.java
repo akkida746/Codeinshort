5639ae3fbb9e70b6304650a7c50c2d9bb500431e
Akash <akkida746@gmail.com>
2020-06-29 11:28:55 +0530

    Intitial commit

---
+++ cqrsspringboot/cqrsspringboot/src/test/java/com/example/cqrsspringboot/CqrsspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cqrsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
