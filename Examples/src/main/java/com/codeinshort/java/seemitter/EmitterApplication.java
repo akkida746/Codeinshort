1f0c27cbeb409511900896814fffeb77a1e6c976
Akash <akkida746@gmail.com>
2020-09-12 10:10:14 +0530

    Emitter file edit

--- Examples/src/main/java/com/codeinshort/java/seemitter/EmitterApplication.java
+++ Examples/src/main/java/com/codeinshort/java/seemitter/EmitterApplication.java
@@ -1,11 +1,22 @@
 package com.codeinshort.java.seemitter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class EmitterApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmitterApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner (){
+        return args -> {
+          log.info("Akash","{} is my name{}","Deep");
+        };
     }
 }
