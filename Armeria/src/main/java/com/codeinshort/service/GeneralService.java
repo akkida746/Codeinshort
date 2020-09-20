429a1e1abeab20ac78744e4ca617c856da16f30d
Akash <akkida746@gmail.com>
2020-09-20 23:03:17 +0530

    Code refactor

--- Armeria/src/main/java/com/codeinshort/service/GeneralService.java
+++ Armeria/src/main/java/com/codeinshort/service/GeneralService.java
@@ -2,11 +2,8 @@ package com.codeinshort.service;
 
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
-import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.server.AbstractHttpService;
-import com.linecorp.armeria.server.HttpService;
 import com.linecorp.armeria.server.ServiceRequestContext;
-import com.linecorp.armeria.server.SimpleDecoratingHttpService;
 
 public class GeneralService extends AbstractHttpService {
 
