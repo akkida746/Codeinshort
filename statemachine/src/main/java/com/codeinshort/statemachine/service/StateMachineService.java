baae3f3376b0c4eef47534ea2cdec29446c52d0e
Akash <akkida746@gmail.com>
2020-10-08 23:06:07 +0530

    Code refactor

--- statemachine/src/main/java/com/codeinshort/statemachine/service/StateMachineService.java
+++ statemachine/src/main/java/com/codeinshort/statemachine/service/StateMachineService.java
@@ -2,7 +2,6 @@ package com.codeinshort.statemachine.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.state.State;
 import org.springframework.stereotype.Service;
 
 @Service
